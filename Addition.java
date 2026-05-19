b1a13cbf9f463fe83f1fb45871a1a2e014661b52
Dhanyajith-coder <dhanyakeerthana391@gmail.com>
2026-05-19 22:45:48 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int numberOne = 5;
+        int numberTwo = 10;
+        int sum = numberOne + numberTwo;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
