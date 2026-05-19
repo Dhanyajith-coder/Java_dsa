8f20966d92b637ff58e8a050b98e88116453a6a8
Dhanyajith-coder <dhanyakeerthana391@gmail.com>
2026-05-19 22:54:49 +0530

    Added sync test print

--- Addition.java
+++ Addition.java
@@ -3,7 +3,7 @@ public class Addition {
         int numberOne = 5;
         int numberTwo = 10;
         int sum = numberOne + numberTwo;
-
+        System.out.println("Testing my GitHub sync!");
         System.out.println("The sum is: " + sum);
     }
 }
