a20a607ab2c6908c92fb8901c42a5fa1a3200ba1
Dhanyajith-coder <dhanyakeerthana391@gmail.com>
2026-09-09 23:58:32 +0530

    Printname

---
+++ Methods/printName.java
@@ -0,0 +1,10 @@
+
+public class printName {
+   static void myName(String name){
+    System.out.println("Myname is "+ name);
+   }
+   public static void main (String[] args){
+    myName("Dhanya");
+    myName("Disha");
+   }
+}
