1d2beb041b3dd05b8e561e1dcbf964fe68d430e9
Dhanyajith-coder <dhanyakeerthana391@gmail.com>
2026-09-17 23:54:14 +0530

    Infinite loops with do while and for loops

---
+++ Basics/InfiniteLoop.java
@@ -0,0 +1,12 @@
+// infinite loop with do while
+package Basics;
+
+public class InfiniteLoop {
+    public static void main(String[] args){
+       do{
+        System.out.println("Byeeee Byeee");
+       }while(true);
+    }
+}
+//with for
+//for(;;)
