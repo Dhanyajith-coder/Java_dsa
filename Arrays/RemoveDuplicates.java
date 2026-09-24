4920a7d9ac967382c65fdd839d380798d7b8721a
Dhanyajith-coder <dhanyakeerthana391@gmail.com>
2026-09-24 23:52:41 +0530

    Removing the duplicate in an sorted array

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,35 @@
+package Arrays;
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the sorted elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int j = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        System.out.println("Array after removing duplicates:");
+
+        for (int i = 0; i <= j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
